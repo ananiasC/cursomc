7c1b2d4470c02a9e4e85968b3f184610d2d52b6b
Ananias Caetano <ananiascaetano@alu.ufc.br>
2020-03-31 15:15:06 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/ananiascaetano/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.ananiascaetano.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ananiascaetano.cursomc.domain.Categoria;
+
+@Repository 
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+}

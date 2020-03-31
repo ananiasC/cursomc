b807d242c4079c43eec19b68e5ea142fca01baef
Ananias Caetano <ananiascaetano@alu.ufc.br>
2020-03-31 16:30:13 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/ananiascaetano/cursomc/services/CategoriaService.java
+++ src/main/java/com/ananiascaetano/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.ananiascaetano.cursomc.domain.Categoria;
 import com.ananiascaetano.cursomc.repositories.CategoriaRepository;
+import com.ananiascaetano.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,8 +15,9 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria find(Integer id) {
+	public Categoria find(Integer id){
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
